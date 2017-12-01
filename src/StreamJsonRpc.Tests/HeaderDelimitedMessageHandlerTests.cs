cb79200ecdab43a71ab59c2fc1ad044ecbb792a4
Andrew Arnott <andrewarnott@gmail.com>
2017-11-30 21:14:38 -0800

    Emit subtype header if it is nonstandard

--- src/StreamJsonRpc.Tests/HeaderDelimitedMessageHandlerTests.cs
+++ src/StreamJsonRpc.Tests/HeaderDelimitedMessageHandlerTests.cs
@@ -87,4 +87,15 @@ CRLF +
         readContent = this.handler.ReadAsync(default(CancellationToken)).GetAwaiter().GetResult();
         Assert.Equal<string>("ABCDEFGHIJ", readContent);
     }
+
+    [Fact]
+    public async Task SubType_ForcesHeader()
+    {
+        this.handler.SubType = "nonstandard";
+        await this.handler.WriteAsync("hello", this.TimeoutToken);
+        this.sendingStream.Position = 0;
+        var sr = new StreamReader(this.sendingStream, this.handler.Encoding);
+        string writtenContent = sr.ReadToEnd();
+        Assert.Contains(this.handler.SubType, writtenContent);
+    }
 }
