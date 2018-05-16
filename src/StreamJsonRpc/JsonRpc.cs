b7e7df2e7b4d4d5333bc76e01462c5132d6f501d
Andrew Arnott <andrewarnott@gmail.com>
2018-05-16 16:14:51 -0700

    Remove the obsolete comment

--- src/StreamJsonRpc/JsonRpc.cs
+++ src/StreamJsonRpc/JsonRpc.cs
@@ -1522,7 +1522,6 @@ namespace StreamJsonRpc
 
             private void OnEventRaised(object sender, EventArgs args)
             {
-                // We use null for the sender because we don't want to try to serialize the target object to the remote party.
                 this.jsonRpc.NotifyAsync(this.eventInfo.Name, new object[] { args });
             }
         }
