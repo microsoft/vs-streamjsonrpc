2a496d965b770f536b67853d1be8b8661c51d5d3
Andrew Arnott <andrewarnott@gmail.com>
2018-09-15 09:08:51 -0700

    Share more between the two message handlers

--- src/StreamJsonRpc/PipeMessageHandler.cs
+++ src/StreamJsonRpc/PipeMessageHandler.cs
@@ -21,24 +21,9 @@ namespace StreamJsonRpc
     /// using <see cref="PipeReader"/> and <see cref="PipeWriter"/>.
     /// </summary>
     /// <typeparam name="T">The type of object to be written/read.</typeparam>
-    /// <remarks>
-    /// This class and its derivatives are safe to call from any thread.
-    /// Read and write requests are protected by a semaphore to guarantee message integrity
-    /// and may be made from any thread.
-    /// </remarks>
-    public abstract class PipeMessageHandler<T> : IDisposableObservable
+    public abstract class PipeMessageHandler<T> : MessageHandlerBase<T>
         where T : class
     {
-        /// <summary>
-        /// A semaphore acquired while sending a message.
-        /// </summary>
-        private readonly AsyncSemaphore sendingSemaphore = new AsyncSemaphore(1);
-
-        /// <summary>
-        /// Backing field for the <see cref="Encoding"/> property.
-        /// </summary>
-        private Encoding encoding;
-
         /// <summary>
         /// Objects that we should dispose when we are disposed. May be null.
         /// </summary>
@@ -61,10 +46,9 @@ namespace StreamJsonRpc
         /// <param name="reader">The reader to use for receiving messages.</param>
         /// <param name="encoding">The encoding to use for transmitted messages.</param>
         public PipeMessageHandler(PipeWriter writer, PipeReader reader, Encoding encoding)
+            : base(Requires.NotNull(encoding, nameof(encoding)))
         {
-            Requires.NotNull(encoding, nameof(encoding));
 
-            this.encoding = encoding;
             this.Reader = reader;
             this.Writer = writer;
         }
@@ -76,11 +60,8 @@ namespace StreamJsonRpc
         /// <param name="reader">The stream to use for receiving messages.</param>
         /// <param name="encoding">The encoding to use for transmitted messages.</param>
         public PipeMessageHandler(Stream writer, Stream reader, Encoding encoding)
+            : base(Requires.NotNull(encoding, nameof(encoding)))
         {
-            Requires.NotNull(encoding, nameof(encoding));
-
-            this.encoding = encoding;
-
             // We use Strict reader to avoid max buffer size issues in Pipe (https://github.com/dotnet/corefx/issues/30689)
             // since it's just stream semantics.
             this.Reader = reader?.UseStrictPipeReader();
@@ -98,33 +79,11 @@ namespace StreamJsonRpc
             }
         }
 
-        /// <summary>
-        /// Gets a value indicating whether this instance has been disposed.
-        /// </summary>
-        public bool IsDisposed { get; private set; }
-
-        /// <summary>
-        /// Gets or sets the encoding to use for transmitted messages.
-        /// </summary>
-        public Encoding Encoding
-        {
-            get
-            {
-                return this.encoding;
-            }
-
-            set
-            {
-                Requires.NotNull(value, nameof(value));
-                this.encoding = value;
-            }
-        }
+        /// <inheritdoc/>
+        public override bool CanRead => this.Reader != null;
 
         /// <inheritdoc/>
-        public bool CanRead => this.Reader != null;
-
-        /// <inheritdoc/>
-        public bool CanWrite => this.Writer != null;
+        public override bool CanWrite => this.Writer != null;
 
         /// <summary>
         /// Gets the reader to use for receiving messages.
@@ -137,55 +96,16 @@ namespace StreamJsonRpc
         protected PipeWriter Writer { get; }
 
         /// <inheritdoc/>
-        public void Dispose()
-        {
-            this.Dispose(true);
-            GC.SuppressFinalize(this);
-        }
+        protected override bool CanFlushConcurrentlyWithOtherWrites => false;
 
 #pragma warning disable AvoidAsyncSuffix // Avoid Async suffix
         /// <inheritdoc/>
-        public virtual ValueTask<T> ReadAsync(CancellationToken cancellationToken)
+        protected override ValueTask WriteCoreAsync(T content, CancellationToken cancellationToken)
         {
-            Verify.Operation(this.CanRead, "No input pipe.");
-            cancellationToken.ThrowIfCancellationRequested();
-            Verify.NotDisposed(this);
-
-            try
-            {
-                return this.ReadCoreAsync(cancellationToken);
-            }
-            catch (InvalidOperationException ex) when (cancellationToken.IsCancellationRequested)
-            {
-                // PipeReader.ReadAsync can throw InvalidOperationException in a race where PipeReader.Complete() has been
-                // called but we haven't noticed the CancellationToken was canceled yet.
-                throw new OperationCanceledException("Reading failed during cancellation.", ex, cancellationToken);
-            }
+            this.Write(content, cancellationToken);
+            return default;
         }
-
-        /// <inheritdoc/>
-        public virtual async ValueTask WriteAsync(T json, CancellationToken cancellationToken)
-        {
-            Requires.NotNull(json, nameof(json));
-            Verify.Operation(this.CanWrite, "No output pipe.");
-            cancellationToken.ThrowIfCancellationRequested();
-            Verify.NotDisposed(this);
-
-            try
-            {
-                using (await this.sendingSemaphore.EnterAsync(cancellationToken).ConfigureAwait(false))
-                {
-                    this.Write(json, cancellationToken);
-                    await this.Writer.FlushAsync(cancellationToken).ConfigureAwait(false);
-                }
-            }
-            catch (ObjectDisposedException)
-            {
-                // If already canceled, throw that instead of ObjectDisposedException.
-                cancellationToken.ThrowIfCancellationRequested();
-                throw;
-            }
-        }
+#pragma warning restore AvoidAsyncSuffix // Avoid Async suffix
 
         /// <summary>
         /// Writes a message to the pipe.
@@ -199,27 +119,11 @@ namespace StreamJsonRpc
         /// </remarks>
         protected abstract void Write(T content, CancellationToken cancellationToken);
 
-        /// <summary>
-        /// Reads a message from the pipe.
-        /// </summary>
-        /// <param name="cancellationToken">A token to cancel the read.</param>
-        /// <returns>A task that represents the asynchronous read operation.</returns>
-        /// <remarks>
-        /// Implementations may assume the method is never called before the previous call has completed.
-        /// </remarks>
-        protected abstract ValueTask<T> ReadCoreAsync(CancellationToken cancellationToken);
-#pragma warning restore AvoidAsyncSuffix // Avoid Async suffix
-
-        /// <summary>
-        /// Disposes of managed and/or native resources.
-        /// </summary>
-        /// <param name="disposing"><c>true</c> to dispose of native resources also.</param>
-        protected virtual void Dispose(bool disposing)
+        /// <inheritdoc />
+        protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
-                this.IsDisposed = true;
-                this.sendingSemaphore.Dispose();
                 this.Reader?.Complete();
                 this.Writer?.Complete();
 
@@ -236,8 +140,13 @@ namespace StreamJsonRpc
                         this.DisposeDisposables();
                     }
                 }
+
+                base.Dispose(disposing);
             }
         }
+
+        /// <inheritdoc />
+        protected override async ValueTask FlushAsync(CancellationToken cancellationToken) => await this.Writer.FlushAsync(cancellationToken).ConfigureAwait(false);
 
         private void DisposeDisposables()
         {
