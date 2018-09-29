microsoft/vs-streamjsonrpc

commit 25a8e35eeb2f8dae4449e7959e51caeeed02f84f

Fault instead of cancel Tasks tracking outbound requests
Specifically, fault the tasks with a ConnectionLostException for this scenario.

Fixes #84

src/StreamJsonRpc/JsonRpc.cs
@@ -1015,7 +1015,7 @@ namespace StreamJsonRpc
                     {
                         if (response == null)
                         {
-                            tcs.TrySetCanceled();
+                            tcs.TrySetException(new ConnectionLostException());
                         }
                         else if (response is JsonRpcError error)
                         {

src/StreamJsonRpc.Tests/JsonRpcWithFatalExceptionsTests.cs
@@ -54,7 +54,7 @@ public class JsonRpcWithFatalExceptionsTests : TestBase
     public async Task CloseStreamsOnSynchronousMethodException()
     {
         var exceptionMessage = "Exception from CloseStreamsOnSynchronousMethodException";
-        OperationCanceledException exception = await Assert.ThrowsAnyAsync<OperationCanceledException>(() => this.clientRpc.InvokeAsync(nameof(Server.MethodThatThrowsUnauthorizedAccessException), exceptionMessage));
+        ConnectionLostException exception = await Assert.ThrowsAnyAsync<ConnectionLostException>(() => this.clientRpc.InvokeAsync(nameof(Server.MethodThatThrowsUnauthorizedAccessException), exceptionMessage));
         Assert.NotNull(exception.StackTrace);
         Assert.Equal(exceptionMessage, this.serverRpc.FaultException.Message);
         Assert.Equal(1, this.serverRpc.IsFatalExceptionCount);
@@ -69,7 +69,7 @@ public class JsonRpcWithFatalExceptionsTests : TestBase
     public async Task CloseStreamOnAsyncYieldAndThrowException()
     {
         var exceptionMessage = "Exception from CloseStreamOnAsyncYieldAndThrowException";
-        Exception exception = await Assert.ThrowsAnyAsync<OperationCanceledException>(() => this.clientRpc.InvokeAsync<string>(nameof(Server.AsyncMethodThatThrowsAfterYield), exceptionMessage));
+        Exception exception = await Assert.ThrowsAnyAsync<ConnectionLostException>(() => this.clientRpc.InvokeAsync<string>(nameof(Server.AsyncMethodThatThrowsAfterYield), exceptionMessage));
         Assert.NotNull(exception.StackTrace);
         Assert.Equal(exceptionMessage, this.serverRpc.FaultException.Message);
         Assert.Equal(1, this.serverRpc.IsFatalExceptionCount);
@@ -81,10 +81,10 @@ public class JsonRpcWithFatalExceptionsTests : TestBase
     }
 
     [Fact]
-    public async Task CloseStreamOnAsyncThrowExceptionandYield()
+    public async Task CloseStreamOnAsyncThrowExceptionAndYield()
     {
         var exceptionMessage = "Exception from CloseStreamOnAsyncThrowExceptionAndYield";
-        Exception exception = await Assert.ThrowsAnyAsync<OperationCanceledException>(() => this.clientRpc.InvokeAsync<string>(nameof(Server.AsyncMethodThatThrowsBeforeYield), exceptionMessage));
+        Exception exception = await Assert.ThrowsAnyAsync<ConnectionLostException>(() => this.clientRpc.InvokeAsync<string>(nameof(Server.AsyncMethodThatThrowsBeforeYield), exceptionMessage));
         Assert.NotNull(exception.StackTrace);
         Assert.Equal(exceptionMessage, this.serverRpc.FaultException.Message);
         Assert.Equal(1, this.serverRpc.IsFatalExceptionCount);
@@ -99,7 +99,7 @@ public class JsonRpcWithFatalExceptionsTests : TestBase
     public async Task CloseStreamOnAsyncTMethodException()
     {
         var exceptionMessage = "Exception from CloseStreamOnAsyncTMethodException";
-        await Assert.ThrowsAnyAsync<OperationCanceledException>(() => this.clientRpc.InvokeAsync<string>(nameof(Server.AsyncMethodThatReturnsStringAndThrows), exceptionMessage));
+        await Assert.ThrowsAnyAsync<ConnectionLostException>(() => this.clientRpc.InvokeAsync<string>(nameof(Server.AsyncMethodThatReturnsStringAndThrows), exceptionMessage));
         Assert.Equal(exceptionMessage, this.serverRpc.FaultException.Message);
         Assert.Equal(1, this.serverRpc.IsFatalExceptionCount);
 
@@ -148,7 +148,7 @@ public class JsonRpcWithFatalExceptionsTests : TestBase
             cts.Cancel();
             this.server.AllowServerMethodToReturn.Set();
 
-            await Assert.ThrowsAnyAsync<OperationCanceledException>(() => invokeTask);
+            await Assert.ThrowsAnyAsync<ConnectionLostException>(() => invokeTask);
             Assert.Equal(Server.ThrowAfterCancellationMessage, this.serverRpc.FaultException.Message);
             Assert.Equal(1, this.serverRpc.IsFatalExceptionCount);
         }

src/StreamJsonRpc/Exceptions/ConnectionLostException.cs
@@ -1,0 +1,59 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace StreamJsonRpc
+{
+    using System;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// An exception used to fault a <see cref="Task"/> returned from a <see cref="JsonRpc"/> request
+    /// when the request could not be completed or the response cannot be received because the connection dropped.
+    /// </summary>
+#if SERIALIZABLE_EXCEPTIONS
+    [System.Serializable]
+#endif
+    public class ConnectionLostException : RemoteRpcException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConnectionLostException"/> class.
+        /// </summary>
+        public ConnectionLostException()
+            : this(Resources.ConnectionDropped)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConnectionLostException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public ConnectionLostException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConnectionLostException"/> class.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner exception is specified.</param>
+        public ConnectionLostException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+#if SERIALIZABLE_EXCEPTIONS
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConnectionLostException"/> class.
+        /// </summary>
+        /// <param name="info">Serialization info.</param>
+        /// <param name="context">Streaming context.</param>
+        protected ConnectionLostException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context)
+        {
+        }
+#endif
+    }
+}

src/StreamJsonRpc/MultilingualResources/StreamJsonRpc.cs.xlf
@@ -200,6 +200,10 @@
           <target state="new">Text encoding is not supported because the formatter "{0}" does not implement "{1}".</target>
           <note from="MultilingualBuild" annotates="source" priority="2">{0} and {1} are CLR type names.</note>
         </trans-unit>
+        <trans-unit id="ConnectionDropped" translate="yes" xml:space="preserve">
+          <source>The JSON-RPC connection with the remote party was lost before the request could complete.</source>
+          <target state="new">The JSON-RPC connection with the remote party was lost before the request could complete.</target>
+        </trans-unit>
       </group>
     </body>
   </file>

src/StreamJsonRpc/MultilingualResources/StreamJsonRpc.de.xlf
@@ -200,6 +200,10 @@
           <target state="new">Text encoding is not supported because the formatter "{0}" does not implement "{1}".</target>
           <note from="MultilingualBuild" annotates="source" priority="2">{0} and {1} are CLR type names.</note>
         </trans-unit>
+        <trans-unit id="ConnectionDropped" translate="yes" xml:space="preserve">
+          <source>The JSON-RPC connection with the remote party was lost before the request could complete.</source>
+          <target state="new">The JSON-RPC connection with the remote party was lost before the request could complete.</target>
+        </trans-unit>
       </group>
     </body>
   </file>

src/StreamJsonRpc/MultilingualResources/StreamJsonRpc.es.xlf
@@ -200,6 +200,10 @@
           <target state="new">Text encoding is not supported because the formatter "{0}" does not implement "{1}".</target>
           <note from="MultilingualBuild" annotates="source" priority="2">{0} and {1} are CLR type names.</note>
         </trans-unit>
+        <trans-unit id="ConnectionDropped" translate="yes" xml:space="preserve">
+          <source>The JSON-RPC connection with the remote party was lost before the request could complete.</source>
+          <target state="new">The JSON-RPC connection with the remote party was lost before the request could complete.</target>
+        </trans-unit>
       </group>
     </body>
   </file>

src/StreamJsonRpc/MultilingualResources/StreamJsonRpc.fr.xlf
@@ -200,6 +200,10 @@
           <target state="new">Text encoding is not supported because the formatter "{0}" does not implement "{1}".</target>
           <note from="MultilingualBuild" annotates="source" priority="2">{0} and {1} are CLR type names.</note>
         </trans-unit>
+        <trans-unit id="ConnectionDropped" translate="yes" xml:space="preserve">
+          <source>The JSON-RPC connection with the remote party was lost before the request could complete.</source>
+          <target state="new">The JSON-RPC connection with the remote party was lost before the request could complete.</target>
+        </trans-unit>
       </group>
     </body>
   </file>

src/StreamJsonRpc/MultilingualResources/StreamJsonRpc.it.xlf
@@ -200,6 +200,10 @@
           <target state="new">Text encoding is not supported because the formatter "{0}" does not implement "{1}".</target>
           <note from="MultilingualBuild" annotates="source" priority="2">{0} and {1} are CLR type names.</note>
         </trans-unit>
+        <trans-unit id="ConnectionDropped" translate="yes" xml:space="preserve">
+          <source>The JSON-RPC connection with the remote party was lost before the request could complete.</source>
+          <target state="new">The JSON-RPC connection with the remote party was lost before the request could complete.</target>
+        </trans-unit>
       </group>
     </body>
   </file>

commit 51c756343c4d803fbb2f457697bf5f979df9372e

More confidently fault aborted requests
The problem was we started shutting things down before faulting requests, such that a race existed in which requests might be "canceled" before we got around to faulting them. So with this change, we fault them *first*, then shut other stuff down.

This should stabilize the JsonRpcWithFatalExceptionsTests tests that are sporatically failing ever since 25a8e35ee was checked in.

src/StreamJsonRpc/JsonRpc.cs
@@ -1482,11 +1482,11 @@ namespace StreamJsonRpc
             }
             finally
             {
-                // Dispose the stream and cancel pending requests in the finally block
+                // Dispose the stream and fault pending requests in the finally block
                 // So this is executed even if Disconnected event handler throws.
                 this.disposeCts.Cancel();
+                this.FaultPendingRequests();
                 (this.MessageHandler as IDisposable)?.Dispose();
-                this.CancelPendingRequests();
 
                 // Ensure the Task we may have returned from Completion is completed.
                 if (eventArgs.Exception != null)
@@ -1697,7 +1697,7 @@ namespace StreamJsonRpc
             }
         }
 
-        private void CancelPendingRequests()
+        private void FaultPendingRequests()
         {
             OutstandingCallData[] pendingRequests;
             lock (this.dispatcherMapLock)

src/StreamJsonRpc.Tests/JsonRpcWithFatalExceptionsTests.cs
@@ -59,10 +59,7 @@ public class JsonRpcWithFatalExceptionsTests : TestBase
         Assert.Equal(exceptionMessage, this.serverRpc.FaultException.Message);
         Assert.Equal(1, this.serverRpc.IsFatalExceptionCount);
 
-        // Assert that the JsonRpc and MessageHandler objects are disposed after exception
-        Assert.True(((IDisposableObservable)this.clientRpc).IsDisposed);
-        Assert.True(((IDisposableObservable)this.serverRpc).IsDisposed);
-        Assert.True(((IDisposableObservable)this.messageHandler).IsDisposed);
+        await this.AssertAllPartiesDisposedEventuallyAsync();
     }
 
     [Fact]
@@ -74,10 +71,7 @@ public class JsonRpcWithFatalExceptionsTests : TestBase
         Assert.Equal(exceptionMessage, this.serverRpc.FaultException.Message);
         Assert.Equal(1, this.serverRpc.IsFatalExceptionCount);
 
-        // Assert that the JsonRpc and MessageHandler objects are disposed after exception
-        Assert.True(((IDisposableObservable)this.clientRpc).IsDisposed);
-        Assert.True(((IDisposableObservable)this.serverRpc).IsDisposed);
-        Assert.True(((IDisposableObservable)this.messageHandler).IsDisposed);
+        await this.AssertAllPartiesDisposedEventuallyAsync();
     }
 
     [Fact]
@@ -89,10 +83,7 @@ public class JsonRpcWithFatalExceptionsTests : TestBase
         Assert.Equal(exceptionMessage, this.serverRpc.FaultException.Message);
         Assert.Equal(1, this.serverRpc.IsFatalExceptionCount);
 
-        // Assert that the JsonRpc and MessageHandler objects are disposed after exception
-        Assert.True(((IDisposableObservable)this.clientRpc).IsDisposed);
-        Assert.True(((IDisposableObservable)this.serverRpc).IsDisposed);
-        Assert.True(((IDisposableObservable)this.messageHandler).IsDisposed);
+        await this.AssertAllPartiesDisposedEventuallyAsync();
     }
 
     [Fact]
@@ -103,10 +94,7 @@ public class JsonRpcWithFatalExceptionsTests : TestBase
         Assert.Equal(exceptionMessage, this.serverRpc.FaultException.Message);
         Assert.Equal(1, this.serverRpc.IsFatalExceptionCount);
 
-        // Assert that the JsonRpc and MessageHandler objects are disposed after exception
-        Assert.True(((IDisposableObservable)this.clientRpc).IsDisposed);
-        Assert.True(((IDisposableObservable)this.serverRpc).IsDisposed);
-        Assert.True(((IDisposableObservable)this.messageHandler).IsDisposed);
+        await this.AssertAllPartiesDisposedEventuallyAsync();
     }
 
     [Fact]
@@ -153,10 +141,7 @@ public class JsonRpcWithFatalExceptionsTests : TestBase
             Assert.Equal(1, this.serverRpc.IsFatalExceptionCount);
         }
 
-        // Assert that the JsonRpc and MessageHandler objects are disposed after exception
-        Assert.True(((IDisposableObservable)this.clientRpc).IsDisposed);
-        Assert.True(((IDisposableObservable)this.serverRpc).IsDisposed);
-        Assert.True(((IDisposableObservable)this.messageHandler).IsDisposed);
+        await this.AssertAllPartiesDisposedEventuallyAsync();
     }
 
     [Fact]
@@ -195,6 +180,39 @@ public class JsonRpcWithFatalExceptionsTests : TestBase
         }
 
         base.Dispose(disposing);
+    }
+
+    private Task AssertAllPartiesDisposedEventuallyAsync()
+    {
+        return Task.WhenAll(
+            this.AssertIsDisposedEventuallyAsync(this.clientRpc),
+            this.AssertIsDisposedEventuallyAsync(this.serverRpc),
+            this.AssertIsDisposedEventuallyAsync(this.messageHandler as IDisposableObservable));
+    }
+
+    private async Task AssertIsDisposedEventuallyAsync(IDisposableObservable disposableObservable)
+    {
+        if (disposableObservable == null)
+        {
+            return;
+        }
+
+        try
+        {
+            while (!this.TimeoutToken.IsCancellationRequested)
+            {
+                if (!disposableObservable.IsDisposed)
+                {
+                    await Task.Delay(1, this.TimeoutToken);
+                }
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            // Swallow this. We're going to assert next for a better failure message.
+        }
+
+        Assert.True(disposableObservable.IsDisposed);
     }
 
     public class Server