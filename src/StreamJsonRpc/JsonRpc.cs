2c5904397912ee7c0c30b6eaa8e002c5799a80e9
Andrew Arnott <andrewarnott@gmail.com>
2019-08-28 16:53:26 -0600

    Slight perf improvement

--- src/StreamJsonRpc/JsonRpc.cs
+++ src/StreamJsonRpc/JsonRpc.cs
@@ -1512,7 +1512,7 @@ namespace StreamJsonRpc
                     // Any exceptions from the relay will be returned back to the origin since we catch all exceptions here.  The message being relayed to the
                     // server would share the same id as the message sent from origin. We just take the message objec wholesale and pass it along to the
                     // other side.
-                    if (localRemoteTargets.Any())
+                    if (!localRemoteTargets.IsEmpty)
                     {
                         if (request.IsResponseExpected)
                         {
