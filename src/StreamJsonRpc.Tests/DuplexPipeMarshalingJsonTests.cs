0521df050e2cc26c67de428f9dd2cbf40edb5f81
Andrew Arnott <andrewarnott@gmail.com>
2019-11-11 13:31:21 -0700

    Prepare for DuplexPipe tests to apply to MessagePackFormatter as well

---
+++ src/StreamJsonRpc.Tests/DuplexPipeMarshalingJsonTests.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using StreamJsonRpc;
+using Xunit.Abstractions;
+
+public class DuplexPipeMarshalingJsonTests : DuplexPipeMarshalingTests
+{
+    public DuplexPipeMarshalingJsonTests(ITestOutputHelper logger)
+        : base(logger)
+    {
+    }
+
+    protected override void InitializeFormattersAndHandlers()
+    {
+        this.serverMessageFormatter = new JsonMessageFormatter { MultiplexingStream = this.serverMx };
+        this.clientMessageFormatter = new JsonMessageFormatter { MultiplexingStream = this.clientMx };
+    }
+}
