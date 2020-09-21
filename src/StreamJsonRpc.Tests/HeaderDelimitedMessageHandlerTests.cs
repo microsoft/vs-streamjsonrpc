microsoft/vs-streamjsonrpc

commit 545c9d03f2376925c3dbb99a0910940780f44819

Bump Nerdbank.Streams from 2.5.76 to 2.6.77 in /src (#559)

src/StreamJsonRpc/PipeMessageHandler.cs
@@ -72,9 +72,7 @@ namespace StreamJsonRpc
         public PipeMessageHandler(Stream? writer, Stream? reader, IJsonRpcMessageFormatter formatter)
             : base(formatter)
         {
-            // We use Strict reader to avoid max buffer size issues in Pipe (https://github.com/dotnet/corefx/issues/30689)
-            // since it's just stream semantics.
-            this.Reader = reader?.UseStrictPipeReader();
+            this.Reader = reader is object ? PipeReader.Create(reader) : null;
             this.Writer = writer?.UsePipeWriter();
 
             // After we've completed writing, only dispose the underlying write stream when we've flushed everything.

src/StreamJsonRpc.Tests/HeaderDelimitedMessageHandlerTests.cs
@@ -5,6 +5,7 @@ using System;
 using System.Buffers;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Pipelines;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -27,7 +28,7 @@ public class HeaderDelimitedMessageHandlerTests : TestBase
         : base(logger)
     {
         // Use strict pipe writer so we get deterministic writes for consistent testing.
-        this.handler = new HeaderDelimitedMessageHandler(this.sendingStream.UseStrictPipeWriter(), this.receivingStream.UseStrictPipeReader(), new JsonMessageFormatter());
+        this.handler = new HeaderDelimitedMessageHandler(PipeWriter.Create(this.sendingStream), PipeReader.Create(this.receivingStream), new JsonMessageFormatter());
     }
 
     [Fact]
@@ -44,7 +45,7 @@ public class HeaderDelimitedMessageHandlerTests : TestBase
     [Fact]
     public void EncodingThrowsForNonTextFormatters()
     {
-        this.handler = new HeaderDelimitedMessageHandler(this.sendingStream.UseStrictPipeWriter(), this.receivingStream.UseStrictPipeReader(), new MockFormatter());
+        this.handler = new HeaderDelimitedMessageHandler(PipeWriter.Create(this.sendingStream), PipeReader.Create(this.receivingStream), new MockFormatter());
         Assert.Throws<NotSupportedException>(() => this.handler.Encoding);
         Assert.Throws<NotSupportedException>(() => this.handler.Encoding = Encoding.UTF8);
     }

src/StreamJsonRpc.Tests/DuplexPipeMarshalingTests.cs
@@ -55,7 +55,7 @@ public abstract class DuplexPipeMarshalingTests : TestBase, IAsyncLifetime
                 new MultiplexingStream.Options
                 {
                     TraceSource = mxServerTraceSource,
-                    DefaultChannelTraceSourceFactory = (id, name) => new TraceSource("MX Server channel " + id, SourceLevels.Verbose) { Listeners = { new XunitTraceListener(this.Logger) } },
+                    DefaultChannelTraceSourceFactoryWithQualifier = (id, name) => new TraceSource("MX Server channel " + id, SourceLevels.Verbose) { Listeners = { new XunitTraceListener(this.Logger) } },
                 },
                 this.TimeoutToken),
             MultiplexingStream.CreateAsync(
@@ -63,7 +63,7 @@ public abstract class DuplexPipeMarshalingTests : TestBase, IAsyncLifetime
                 new MultiplexingStream.Options
                 {
                     TraceSource = mxClientTraceSource,
-                    DefaultChannelTraceSourceFactory = (id, name) => new TraceSource("MX Client channel " + id, SourceLevels.Verbose) { Listeners = { new XunitTraceListener(this.Logger) } },
+                    DefaultChannelTraceSourceFactoryWithQualifier = (id, name) => new TraceSource("MX Client channel " + id, SourceLevels.Verbose) { Listeners = { new XunitTraceListener(this.Logger) } },
                 },
                 this.TimeoutToken));
         this.serverMx = mxStreams[0];
@@ -429,7 +429,7 @@ public abstract class DuplexPipeMarshalingTests : TestBase, IAsyncLifetime
             {
                 RequestId = new RequestId(1),
                 Method = nameof(ServerWithOverloads.OverloadedMethod),
-                ArgumentsList = new object[] { false, oobChannel.Id, new object() },
+                ArgumentsList = new object[] { false, oobChannel.QualifiedId.Id, new object() },
             },
             this.TimeoutToken);
         await clientRpcChannel.Output.FlushAsync(this.TimeoutToken);
@@ -563,10 +563,10 @@ public abstract class DuplexPipeMarshalingTests : TestBase, IAsyncLifetime
     [Fact]
     public async Task InvokeWithPipe_ServerMethodDoesNotExist_ChannelOfferCanceled()
     {
-        int? channelIdOffered = null;
+        ulong? channelIdOffered = null;
         this.serverMx.ChannelOffered += (s, e) =>
         {
-            channelIdOffered = e.Id;
+            channelIdOffered = e.QualifiedId.Id;
         };
         (IDuplexPipe, IDuplexPipe) duplexPipes = FullDuplexStream.CreatePipePair();
         await Assert.ThrowsAsync<RemoteMethodNotFoundException>(() => this.clientRpc.InvokeWithCancellationAsync("does not exist", new object[] { duplexPipes.Item2 }, this.TimeoutToken));

src/StreamJsonRpc/JsonMessageFormatter.cs
@@ -1108,13 +1108,13 @@ namespace StreamJsonRpc
 
             public override IDuplexPipe? ReadJson(JsonReader reader, Type objectType, IDuplexPipe? existingValue, bool hasExistingValue, JsonSerializer serializer)
             {
-                int? tokenId = JToken.Load(reader).Value<int?>();
+                ulong? tokenId = JToken.Load(reader).Value<ulong?>();
                 return this.jsonMessageFormatter.duplexPipeTracker!.GetPipe(tokenId);
             }
 
             public override void WriteJson(JsonWriter writer, IDuplexPipe? value, JsonSerializer serializer)
             {
-                var token = this.jsonMessageFormatter.duplexPipeTracker!.GetToken(value);
+                ulong? token = this.jsonMessageFormatter.duplexPipeTracker!.GetULongToken(value);
                 writer.WriteValue(token);
             }
         }
@@ -1130,13 +1130,13 @@ namespace StreamJsonRpc
 
             public override PipeReader? ReadJson(JsonReader reader, Type objectType, PipeReader? existingValue, bool hasExistingValue, JsonSerializer serializer)
             {
-                int? tokenId = JToken.Load(reader).Value<int?>();
+                ulong? tokenId = JToken.Load(reader).Value<ulong?>();
                 return this.jsonMessageFormatter.DuplexPipeTracker.GetPipeReader(tokenId);
             }
 
             public override void WriteJson(JsonWriter writer, PipeReader? value, JsonSerializer serializer)
             {
-                var token = this.jsonMessageFormatter.DuplexPipeTracker.GetToken(value);
+                ulong? token = this.jsonMessageFormatter.DuplexPipeTracker.GetULongToken(value);
                 writer.WriteValue(token);
             }
         }
@@ -1152,13 +1152,13 @@ namespace StreamJsonRpc
 
             public override PipeWriter? ReadJson(JsonReader reader, Type objectType, PipeWriter? existingValue, bool hasExistingValue, JsonSerializer serializer)
             {
-                int? tokenId = JToken.Load(reader).Value<int?>();
+                ulong? tokenId = JToken.Load(reader).Value<ulong?>();
                 return this.jsonMessageFormatter.DuplexPipeTracker.GetPipeWriter(tokenId);
             }
 
             public override void WriteJson(JsonWriter writer, PipeWriter? value, JsonSerializer serializer)
             {
-                var token = this.jsonMessageFormatter.DuplexPipeTracker.GetToken(value);
+                ulong? token = this.jsonMessageFormatter.DuplexPipeTracker.GetULongToken(value);
                 writer.WriteValue(token);
             }
         }
@@ -1174,13 +1174,13 @@ namespace StreamJsonRpc
 
             public override Stream? ReadJson(JsonReader reader, Type objectType, Stream? existingValue, bool hasExistingValue, JsonSerializer serializer)
             {
-                int? tokenId = JToken.Load(reader).Value<int?>();
+                ulong? tokenId = JToken.Load(reader).Value<ulong?>();
                 return this.jsonMessageFormatter.DuplexPipeTracker.GetPipe(tokenId)?.AsStream();
             }
 
             public override void WriteJson(JsonWriter writer, Stream? value, JsonSerializer serializer)
             {
-                var token = this.jsonMessageFormatter.DuplexPipeTracker.GetToken(value?.UsePipe());
+                ulong? token = this.jsonMessageFormatter.DuplexPipeTracker.GetULongToken(value?.UsePipe());
                 writer.WriteValue(token);
             }
         }

src/StreamJsonRpc/MessagePackFormatter.cs
@@ -990,12 +990,12 @@ namespace StreamJsonRpc
                         return null;
                     }
 
-                    return (T)this.formatter.DuplexPipeTracker.GetPipe(reader.ReadInt32());
+                    return (T)this.formatter.DuplexPipeTracker.GetPipe(reader.ReadUInt64());
                 }
 
                 public void Serialize(ref MessagePackWriter writer, T? value, MessagePackSerializerOptions options)
                 {
-                    if (this.formatter.DuplexPipeTracker.GetToken(value) is { } token)
+                    if (this.formatter.DuplexPipeTracker.GetULongToken(value) is { } token)
                     {
                         writer.Write(token);
                     }
@@ -1023,12 +1023,12 @@ namespace StreamJsonRpc
                         return null;
                     }
 
-                    return (T)this.formatter.DuplexPipeTracker.GetPipeReader(reader.ReadInt32());
+                    return (T)this.formatter.DuplexPipeTracker.GetPipeReader(reader.ReadUInt64());
                 }
 
                 public void Serialize(ref MessagePackWriter writer, T? value, MessagePackSerializerOptions options)
                 {
-                    if (this.formatter.DuplexPipeTracker.GetToken(value) is { } token)
+                    if (this.formatter.DuplexPipeTracker.GetULongToken(value) is { } token)
                     {
                         writer.Write(token);
                     }
@@ -1056,12 +1056,12 @@ namespace StreamJsonRpc
                         return null;
                     }
 
-                    return (T)this.formatter.DuplexPipeTracker.GetPipeWriter(reader.ReadInt32());
+                    return (T)this.formatter.DuplexPipeTracker.GetPipeWriter(reader.ReadUInt64());
                 }
 
                 public void Serialize(ref MessagePackWriter writer, T? value, MessagePackSerializerOptions options)
                 {
-                    if (this.formatter.DuplexPipeTracker.GetToken(value) is { } token)
+                    if (this.formatter.DuplexPipeTracker.GetULongToken(value) is { } token)
                     {
                         writer.Write(token);
                     }
@@ -1089,12 +1089,12 @@ namespace StreamJsonRpc
                         return null;
                     }
 
-                    return (T)this.formatter.DuplexPipeTracker.GetPipe(reader.ReadInt32()).AsStream();
+                    return (T)this.formatter.DuplexPipeTracker.GetPipe(reader.ReadUInt64()).AsStream();
                 }
 
                 public void Serialize(ref MessagePackWriter writer, T? value, MessagePackSerializerOptions options)
                 {
-                    if (this.formatter.DuplexPipeTracker.GetToken(value?.UsePipe()) is { } token)
+                    if (this.formatter.DuplexPipeTracker.GetULongToken(value?.UsePipe()) is { } token)
                     {
                         writer.Write(token);
                     }

src/StreamJsonRpc/Reflection/MessageFormatterDuplexPipeTracker.cs
@@ -7,7 +7,6 @@ namespace StreamJsonRpc.Reflection
     using System.Collections.Generic;
     using System.Collections.Immutable;
     using System.Diagnostics.CodeAnalysis;
-    using System.IO;
     using System.IO.Pipelines;
     using System.Threading;
     using System.Threading.Tasks;
@@ -47,12 +46,12 @@ namespace StreamJsonRpc.Reflection
         /// <summary>
         /// The set of channels that have been opened but not yet closed to support outbound requests, keyed by their ID.
         /// </summary>
-        private ImmutableDictionary<int, MultiplexingStream.Channel> openOutboundChannels = ImmutableDictionary<int, MultiplexingStream.Channel>.Empty;
+        private ImmutableDictionary<MultiplexingStream.QualifiedChannelId, MultiplexingStream.Channel> openOutboundChannels = ImmutableDictionary<MultiplexingStream.QualifiedChannelId, MultiplexingStream.Channel>.Empty;
 
         /// <summary>
         /// The set of channels that have been opened but not yet closed to support inbound requests, keyed by their ID.
         /// </summary>
-        private ImmutableDictionary<int, MultiplexingStream.Channel> openInboundChannels = ImmutableDictionary<int, MultiplexingStream.Channel>.Empty;
+        private ImmutableDictionary<MultiplexingStream.QualifiedChannelId, MultiplexingStream.Channel> openInboundChannels = ImmutableDictionary<MultiplexingStream.QualifiedChannelId, MultiplexingStream.Channel>.Empty;
 
         /// <summary>
         /// Backing field for the <see cref="IDisposableObservable.IsDisposed"/> property.
@@ -99,6 +98,11 @@ namespace StreamJsonRpc.Reflection
         /// </summary>
         private RequestId RequestIdBeingDeserialized => this.formatterState.DeserializingMessageWithId;
 
+        /// <inheritdoc cref="GetULongToken(IDuplexPipe?)"/>
+        [return: NotNullIfNotNull("duplexPipe")]
+        [Obsolete("Use " + nameof(GetULongToken) + " instead.")]
+        public int? GetToken(IDuplexPipe? duplexPipe) => checked((int?)this.GetULongToken(duplexPipe));
+
         /// <summary>
         /// Creates a token to represent an <see cref="IDuplexPipe"/> as it is transmitted from the client to an RPC server as a method argument.
         /// </summary>
@@ -106,7 +110,7 @@ namespace StreamJsonRpc.Reflection
         /// <returns>The token to use as the RPC method argument; or <c>null</c> if <paramref name="duplexPipe"/> was <c>null</c>.</returns>
         /// <exception cref="NotSupportedException">Thrown if no <see cref="MultiplexingStream"/> was provided to the constructor or when serializing a message without an ID property.</exception>
         [return: NotNullIfNotNull("duplexPipe")]
-        public int? GetToken(IDuplexPipe? duplexPipe)
+        public ulong? GetULongToken(IDuplexPipe? duplexPipe)
         {
             Verify.NotDisposed(this);
 
@@ -136,11 +140,16 @@ namespace StreamJsonRpc.Reflection
             }
 
             // Track open channels to assist in diagnosing abandoned channels.
-            ImmutableInterlocked.TryAdd(ref this.openOutboundChannels, channel.Id, channel);
-            channel.Completion.ContinueWith(_ => ImmutableInterlocked.TryRemove(ref this.openOutboundChannels, channel.Id, out MultiplexingStream.Channel removedChannel), CancellationToken.None, TaskContinuationOptions.None, TaskScheduler.Default).Forget();
+            ImmutableInterlocked.TryAdd(ref this.openOutboundChannels, channel.QualifiedId, channel);
+            channel.Completion.ContinueWith(_ => ImmutableInterlocked.TryRemove(ref this.openOutboundChannels, channel.QualifiedId, out MultiplexingStream.Channel removedChannel), CancellationToken.None, TaskContinuationOptions.None, TaskScheduler.Default).Forget();
 
-            return channel.Id;
+            return channel.QualifiedId.Id;
         }
+
+        /// <inheritdoc cref="GetULongToken(PipeReader?)"/>
+        [return: NotNullIfNotNull("reader")]
+        [Obsolete("Use " + nameof(GetULongToken) + " instead.")]
+        public int? GetToken(PipeReader? reader) => checked((int?)this.GetULongToken(reader));
 
         /// <summary>
         /// Creates a token to represent a <see cref="PipeReader"/> as it is transmitted from the client to an RPC server as a method argument.
@@ -149,7 +158,12 @@ namespace StreamJsonRpc.Reflection
         /// <returns>The token to use as the RPC method argument; or <c>null</c> if <paramref name="reader"/> was <c>null</c>.</returns>
         /// <exception cref="NotSupportedException">Thrown if no <see cref="MultiplexingStream"/> was provided to the constructor or when serializing a message without an ID property.</exception>
         [return: NotNullIfNotNull("reader")]
-        public int? GetToken(PipeReader? reader) => this.GetToken(reader != null ? new DuplexPipe(reader) : null);
+        public ulong? GetULongToken(PipeReader? reader) => this.GetULongToken(reader != null ? new DuplexPipe(reader) : null);
+
+        /// <inheritdoc cref="GetULongToken(PipeWriter?)"/>
+        [return: NotNullIfNotNull("writer")]
+        [Obsolete("Use " + nameof(GetULongToken) + " instead.")]
+        public int? GetToken(PipeWriter? writer) => checked((int?)this.GetULongToken(writer));
 
         /// <summary>
         /// Creates a token to represent a <see cref="PipeWriter"/> as it is transmitted from the client to an RPC server as a method argument.
@@ -158,7 +172,12 @@ namespace StreamJsonRpc.Reflection
         /// <returns>The token to use as the RPC method argument; or <c>null</c> if <paramref name="writer"/> was <c>null</c>.</returns>
         /// <exception cref="NotSupportedException">Thrown if no <see cref="MultiplexingStream"/> was provided to the constructor or when serializing a message without an ID property.</exception>
         [return: NotNullIfNotNull("writer")]
-        public int? GetToken(PipeWriter? writer) => this.GetToken(writer != null ? new DuplexPipe(writer) : null);
+        public ulong? GetULongToken(PipeWriter? writer) => this.GetULongToken(writer != null ? new DuplexPipe(writer) : null);
+
+        /// <inheritdoc cref="GetPipe(ulong?)"/>
+        [return: NotNullIfNotNull("token")]
+        [Obsolete("Use " + nameof(GetPipe) + "(ulong?) instead.")]
+        public IDuplexPipe? GetPipe(int? token) => this.GetPipe((ulong?)token);
 
         /// <summary>
         /// Creates an <see cref="IDuplexPipe"/> from a given token as it is received at the RPC server as a method argument.
@@ -168,7 +187,7 @@ namespace StreamJsonRpc.Reflection
         /// <exception cref="InvalidOperationException">Thrown if the token does not match up with an out of band channel offered by the client.</exception>
         /// <exception cref="NotSupportedException">Thrown if no <see cref="MultiplexingStream"/> was provided to the constructor.</exception>
         [return: NotNullIfNotNull("token")]
-        public IDuplexPipe? GetPipe(int? token)
+        public IDuplexPipe? GetPipe(ulong? token)
         {
             Verify.NotDisposed(this);
 
@@ -180,7 +199,7 @@ namespace StreamJsonRpc.Reflection
 
             // In the case of multiple overloads, we might be called to convert a channel's token more than once.
             // But we can only accept the channel once, so look up in a dictionary to see if we've already done this.
-            if (!this.openInboundChannels.TryGetValue(token.Value, out MultiplexingStream.Channel channel))
+            if (!this.openInboundChannels.TryGetValue(new MultiplexingStream.QualifiedChannelId(token.Value, MultiplexingStream.ChannelSource.Remote), out MultiplexingStream.Channel channel))
             {
                 channel = mxstream.AcceptChannel(token.Value);
                 if (!this.RequestIdBeingDeserialized.IsEmpty)
@@ -193,12 +212,17 @@ namespace StreamJsonRpc.Reflection
                 }
 
                 // Track open channels to assist in diagnosing abandoned channels and handling multiple overloads.
-                ImmutableInterlocked.TryAdd(ref this.openInboundChannels, channel.Id, channel);
-                channel.Completion.ContinueWith(_ => ImmutableInterlocked.TryRemove(ref this.openInboundChannels, channel.Id, out MultiplexingStream.Channel removedChannel), CancellationToken.None, TaskContinuationOptions.None, TaskScheduler.Default).Forget();
+                ImmutableInterlocked.TryAdd(ref this.openInboundChannels, channel.QualifiedId, channel);
+                channel.Completion.ContinueWith(_ => ImmutableInterlocked.TryRemove(ref this.openInboundChannels, channel.QualifiedId, out MultiplexingStream.Channel removedChannel), CancellationToken.None, TaskContinuationOptions.None, TaskScheduler.Default).Forget();
             }
 
             return channel;
         }
+
+        /// <inheritdoc cref="GetPipeReader(ulong?)"/>
+        [return: NotNullIfNotNull("token")]
+        [Obsolete("Use " + nameof(GetPipeReader) + "(ulong?) instead.")]
+        public PipeReader? GetPipeReader(int? token) => this.GetPipeReader((ulong?)token);
 
         /// <summary>
         /// Creates a <see cref="PipeReader"/> from a given token as it is received at the RPC server as a method argument.
@@ -208,7 +232,7 @@ namespace StreamJsonRpc.Reflection
         /// <exception cref="InvalidOperationException">Thrown if the token does not match up with an out of band channel offered by the client.</exception>
         /// <exception cref="NotSupportedException">Thrown if no <see cref="MultiplexingStream"/> was provided to the constructor.</exception>
         [return: NotNullIfNotNull("token")]
-        public PipeReader? GetPipeReader(int? token)
+        public PipeReader? GetPipeReader(ulong? token)
         {
             IDuplexPipe? duplexPipe = this.GetPipe(token);
             if (duplexPipe != null)
@@ -220,6 +244,11 @@ namespace StreamJsonRpc.Reflection
             return null;
         }
 
+        /// <inheritdoc cref="GetPipeWriter(ulong?)"/>
+        [return: NotNullIfNotNull("token")]
+        [Obsolete("Use " + nameof(GetPipeWriter) + "(ulong?) instead.")]
+        public PipeWriter? GetPipeWriter(int? token) => this.GetPipeWriter((ulong?)token);
+
         /// <summary>
         /// Creates a <see cref="PipeWriter"/> from a given token as it is received at the RPC server as a method argument.
         /// </summary>
@@ -228,7 +257,7 @@ namespace StreamJsonRpc.Reflection
         /// <exception cref="InvalidOperationException">Thrown if the token does not match up with an out of band channel offered by the client.</exception>
         /// <exception cref="NotSupportedException">Thrown if no <see cref="MultiplexingStream"/> was provided to the constructor.</exception>
         [return: NotNullIfNotNull("token")]
-        public PipeWriter? GetPipeWriter(int? token)
+        public PipeWriter? GetPipeWriter(ulong? token)
         {
             IDuplexPipe? duplexPipe = this.GetPipe(token);
             if (duplexPipe != null)
@@ -249,14 +278,14 @@ namespace StreamJsonRpc.Reflection
             this.outboundRequestChannelMap = this.outboundRequestChannelMap.Clear();
             this.inboundRequestChannelMap = this.inboundRequestChannelMap.Clear();
 
-            ImmutableDictionary<int, MultiplexingStream.Channel> openInboundChannels = Interlocked.Exchange(ref this.openInboundChannels, this.openInboundChannels.Clear());
-            foreach (KeyValuePair<int, MultiplexingStream.Channel> entry in openInboundChannels)
+            ImmutableDictionary<MultiplexingStream.QualifiedChannelId, MultiplexingStream.Channel> openInboundChannels = Interlocked.Exchange(ref this.openInboundChannels, this.openInboundChannels.Clear());
+            foreach (KeyValuePair<MultiplexingStream.QualifiedChannelId, MultiplexingStream.Channel> entry in openInboundChannels)
             {
                 entry.Value.Dispose();
             }
 
-            ImmutableDictionary<int, MultiplexingStream.Channel> openOutboundChannels = Interlocked.Exchange(ref this.openOutboundChannels, this.openOutboundChannels.Clear());
-            foreach (KeyValuePair<int, MultiplexingStream.Channel> entry in openOutboundChannels)
+            ImmutableDictionary<MultiplexingStream.QualifiedChannelId, MultiplexingStream.Channel> openOutboundChannels = Interlocked.Exchange(ref this.openOutboundChannels, this.openOutboundChannels.Clear());
+            foreach (KeyValuePair<MultiplexingStream.QualifiedChannelId, MultiplexingStream.Channel> entry in openOutboundChannels)
             {
                 entry.Value.Dispose();
             }

src/StreamJsonRpc/StreamJsonRpc.csproj
@@ -22,7 +22,7 @@
     <PackageReference Include="Microsoft.VisualStudio.Threading.Analyzers" Version="16.7.56" PrivateAssets="all" />
     <PackageReference Include="Microsoft.VisualStudio.Threading" Version="16.7.56" />
     <PackageReference Include="Microsoft.Bcl.AsyncInterfaces" Version="1.1.1" PrivateAssets="compile" />
-    <PackageReference Include="Nerdbank.Streams" Version="2.5.76" />
+    <PackageReference Include="Nerdbank.Streams" Version="2.6.77" />
     <PackageReference Include="Newtonsoft.Json" Version="12.0.2" />
     <PackageReference Include="Microsoft.CodeAnalysis.PublicApiAnalyzers" Version="3.3.0" PrivateAssets="all" />
     <PackageReference Include="System.Collections.Immutable" Version="1.7.1" />

commit 274d12b42e61fa6d18346b43b2f17223c52d1df9

Do not close read stream too soon
This fixes a stability regression introduced in #559 which was detected by the `MessageHeaderTests`.

src/StreamJsonRpc/PipeMessageHandler.cs
@@ -72,7 +72,7 @@ namespace StreamJsonRpc
         public PipeMessageHandler(Stream? writer, Stream? reader, IJsonRpcMessageFormatter formatter)
             : base(formatter)
         {
-            this.Reader = reader is object ? PipeReader.Create(reader) : null;
+            this.Reader = reader is object ? PipeReader.Create(reader, new StreamPipeReaderOptions(leaveOpen: true)) : null;
             this.Writer = writer?.UsePipeWriter();
 
             // After we've completed writing, only dispose the underlying write stream when we've flushed everything.

src/StreamJsonRpc.Tests/HeaderDelimitedMessageHandlerTests.cs
@@ -28,7 +28,10 @@ public class HeaderDelimitedMessageHandlerTests : TestBase
         : base(logger)
     {
         // Use strict pipe writer so we get deterministic writes for consistent testing.
-        this.handler = new HeaderDelimitedMessageHandler(PipeWriter.Create(this.sendingStream), PipeReader.Create(this.receivingStream), new JsonMessageFormatter());
+        this.handler = new HeaderDelimitedMessageHandler(
+            PipeWriter.Create(this.sendingStream, new StreamPipeWriterOptions(leaveOpen: true)),
+            PipeReader.Create(this.receivingStream, new StreamPipeReaderOptions(leaveOpen: true)),
+            new JsonMessageFormatter());
     }
 
     [Fact]
@@ -45,7 +48,10 @@ public class HeaderDelimitedMessageHandlerTests : TestBase
     [Fact]
     public void EncodingThrowsForNonTextFormatters()
     {
-        this.handler = new HeaderDelimitedMessageHandler(PipeWriter.Create(this.sendingStream), PipeReader.Create(this.receivingStream), new MockFormatter());
+        this.handler = new HeaderDelimitedMessageHandler(
+            PipeWriter.Create(this.sendingStream, new StreamPipeWriterOptions(leaveOpen: true)),
+            PipeReader.Create(this.receivingStream, new StreamPipeReaderOptions(leaveOpen: true)),
+            new MockFormatter());
         Assert.Throws<NotSupportedException>(() => this.handler.Encoding);
         Assert.Throws<NotSupportedException>(() => this.handler.Encoding = Encoding.UTF8);
     }