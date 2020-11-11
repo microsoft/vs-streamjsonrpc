e4b4ce891dd580d7f4f8a58f47ad802a81168b4f
Andrew Arnott <andrewarnott@gmail.com>
2020-11-10 19:19:47 -0700

    Add netcoreapp2.1 to benchmark

--- test/Benchmarks/run.cmd
+++ test/Benchmarks/run.cmd
@@ -1,3 +1,3 @@
 @pushd "%~dp0\"
-dotnet run -f netcoreapp3.1 -c release -- --runtimes net472 netcoreapp3.1 %*
+dotnet run -f netcoreapp3.1 -c release -- --runtimes net472 netcoreapp2.1 netcoreapp3.1 %*
 @popd
