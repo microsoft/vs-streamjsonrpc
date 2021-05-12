ccb5c03b645310aebfb01b793c34d731635672b5
Andrew Arnott <andrewarnott@live.com>
2021-05-11 22:46:38 -0600

    Reapplies "Merge pull request #652 from rruizGit/dev/rruiz/pr637"
    This reverts commit 8cd2c4a5b79087dc7005b2ea4dd0be633ca41d62.

--- src/StreamJsonRpc/Reflection/RpcTargetInfo.cs
+++ src/StreamJsonRpc/Reflection/RpcTargetInfo.cs
@@ -358,7 +358,27 @@ namespace StreamJsonRpc.Reflection
 
             MethodNameMap mapping = GetMethodNameMap(exposedMembersOnType);
 
-            for (TypeInfo? t = exposedMembersOnType; t != null && t != typeof(object).GetTypeInfo(); t = t.BaseType?.GetTypeInfo())
+            // We retrieve exposed types differently for interfaces vs. classes
+            var typesToMap = new List<TypeInfo>();
+            if (exposedMembersOnType.IsInterface)
+            {
+                Type[] ifaces = exposedMembersOnType.GetInterfaces();
+                typesToMap.Capacity = 1 + ifaces.Length;
+                typesToMap.Add(exposedMembersOnType.GetTypeInfo());
+                foreach (Type iface in ifaces)
+                {
+                    typesToMap.Add(iface.GetTypeInfo());
+                }
+            }
+            else
+            {
+                for (TypeInfo? t = exposedMembersOnType.GetTypeInfo(); t != null && t != typeof(object).GetTypeInfo(); t = t.BaseType?.GetTypeInfo())
+                {
+                    typesToMap.Add(t);
+                }
+            }
+
+            foreach (TypeInfo t in typesToMap)
             {
                 // As we enumerate methods, skip accessor methods
                 foreach (MethodInfo method in t.DeclaredMethods.Where(m => !m.IsSpecialName))
