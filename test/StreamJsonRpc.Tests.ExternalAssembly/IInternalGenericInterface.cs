af40eb6474368104d7cf5a654d1c1d1534c3e087
Andrew Arnott <andrew.arnott@microsoft.com>
2022-06-09 20:16:48 -0600

    Add test for more complex internal type references in generated proxy
    This repros #789

---
+++ test/StreamJsonRpc.Tests.ExternalAssembly/IInternalGenericInterface.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace StreamJsonRpc.Tests.ExternalAssembly
+{
+    using System.Threading.Tasks;
+
+    internal interface IInternalGenericInterface<TOptions>
+    {
+        Task<TOptions> GetOptionsAsync(InternalStruct id, CancellationToken cancellationToken);
+    }
+}
