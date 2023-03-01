2d9f225c3609dc89c479adc5d4ce6e8ef0f563d9
Andrew Arnott <andrew.arnott@microsoft.com>
2023-03-01 08:53:45 -0700

    Move VSInsertionMetadata package to .props files

--- azure-pipelines/variables/InsertConfigValues.ps1
+++ azure-pipelines/variables/InsertConfigValues.ps1
@@ -1,8 +1,14 @@
-$InsertedPkgs = (& "$PSScriptRoot\..\artifacts\VSInsertion.ps1")
+$BuildConfiguration = $env:BUILDCONFIGURATION
+if (!$BuildConfiguration) {
+    $BuildConfiguration = 'Debug'
+}
+$BinPath = [System.IO.Path]::GetFullPath("$PSScriptRoot\..\..\bin\Packages\$BuildConfiguration")
 
+$dirsToSearch = "$BinPath\NuGet\*.nupkg" |? { Test-Path $_ }
 $icv=@()
-foreach ($kvp in $InsertedPkgs.GetEnumerator()) {
-    $kvp.Value |% {
+
+if ($dirsToSearch) {
+    Get-ChildItem -Path $dirsToSearch |% {
         if ($_.Name -match "^(.*?)\.(\d+\.\d+\.\d+(?:\.\d+)?(?:-.*?)?)(?:\.symbols)?\.nupkg$") {
             $id = $Matches[1]
             $version = $Matches[2]
--- azure-pipelines/variables/InsertPropsValues.ps1
+++ azure-pipelines/variables/InsertPropsValues.ps1
@@ -1,13 +1,9 @@
-$BinPath = [System.IO.Path]::GetFullPath("$PSScriptRoot\..\..\bin\Packages\$env:BUILDCONFIGURATION")
+$InsertedPkgs = (& "$PSScriptRoot\..\artifacts\VSInsertion.ps1")
 
-$dirsToSearch = "$BinPath\NuGet\*.nupkg" |? { Test-Path $_ }
 $icv=@()
-
-# Avoid updating the .props world for an optprof run.
-# This avoids breaks in servicing branches when we insert a PublicRelease=false unstable package version.
-if ($dirsToSearch -and !$env:OPTPROF) {
-    Get-ChildItem -Path $dirsToSearch |% {
-        if ($_.Name -match "^(.*)\.(\d+\.\d+\.\d+(?:-.*?)?)(?:\.symbols)?\.nupkg$") {
+foreach ($kvp in $InsertedPkgs.GetEnumerator()) {
+    $kvp.Value |% {
+        if ($_.Name -match "^(.*?)\.(\d+\.\d+\.\d+(?:\.\d+)?(?:-.*?)?)(?:\.symbols)?\.nupkg$") {
             $id = $Matches[1]
             $version = $Matches[2]
             $icv += "$id=$version"
