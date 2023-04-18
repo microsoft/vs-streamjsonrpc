a5f8362c0c81614e56fbbabe7625ae7f592b1a0c
Andrew Arnott <andrew.arnott@microsoft.com>
2023-04-17 21:21:32 -0600

    Early System.Text.Json formatter work

---
+++ src/StreamJsonRpc/SystemTextJsonFormatter.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Buffers;
+using System.Text;
+using System.Text.Json;
+using StreamJsonRpc.Protocol;
+
+namespace StreamJsonRpc;
+
+public class SystemTextJsonFormatter : IJsonRpcMessageFormatter, IJsonRpcMessageTextFormatter
+{
+    private static JsonWriterOptions WriterOptions = new();
+    private static JsonReaderOptions ReaderOptions = new();
+
+    private static JsonSerializerOptions SerializerOptions = new();
+
+    /// <summary>
+    /// UTF-8 encoding without a preamble.
+    /// </summary>
+    private static readonly Encoding DefaultEncoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false);
+
+    public Encoding Encoding
+    {
+        get => DefaultEncoding;
+        set => throw new NotSupportedException();
+    }
+
+    public JsonRpcMessage Deserialize(ReadOnlySequence<byte> contentBuffer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public JsonRpcMessage Deserialize(ReadOnlySequence<byte> contentBuffer, Encoding encoding)
+    {
+        if (encoding is not UTF8Encoding)
+        {
+            throw new NotSupportedException("Only our default encoding is supported.");
+        }
+
+        Utf8JsonReader reader = new(contentBuffer, ReaderOptions);
+        return JsonSerializer.Deserialize<JsonRpcRequest>(ref reader, SerializerOptions) ?? throw new Exception("Empty message");
+    }
+
+    public object GetJsonText(JsonRpcMessage message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Serialize(IBufferWriter<byte> bufferWriter, JsonRpcMessage message)
+    {
+        using Utf8JsonWriter writer = new(bufferWriter, WriterOptions);
+        JsonSerializer.Serialize(writer, message, SerializerOptions);
+    }
+}
---
+++ test/StreamJsonRpc.Tests/JsonRpcSystemTextJsonHeadersTests.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Runtime.Serialization;
+using System.Text;
+using Microsoft.VisualStudio.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using StreamJsonRpc;
+using StreamJsonRpc.Protocol;
+using Xunit;
+using Xunit.Abstractions;
+
+public class JsonRpcSystemTextJsonHeadersTests : JsonRpcTests
+{
+    public JsonRpcSystemTextJsonHeadersTests(ITestOutputHelper logger)
+        : base(logger)
+    {
+    }
+
+    protected override void InitializeFormattersAndHandlers(bool controlledFlushingClient)
+    {
+        this.clientMessageFormatter = new SystemTextJsonFormatter
+        {
+        };
+        this.serverMessageFormatter = new SystemTextJsonFormatter
+        {
+        };
+
+        this.serverMessageHandler = new HeaderDelimitedMessageHandler(this.serverStream, this.serverStream, this.serverMessageFormatter);
+        this.clientMessageHandler = controlledFlushingClient
+            ? new DelayedFlushingHandler(this.clientStream, this.clientMessageFormatter)
+            : new HeaderDelimitedMessageHandler(this.clientStream, this.clientStream, this.clientMessageFormatter);
+    }
+
+    protected class DelayedFlushingHandler : HeaderDelimitedMessageHandler, IControlledFlushHandler
+    {
+        public DelayedFlushingHandler(Stream stream, IJsonRpcMessageFormatter formatter)
+            : base(stream, formatter)
+        {
+        }
+
+        public AsyncAutoResetEvent FlushEntered { get; } = new AsyncAutoResetEvent();
+
+        public AsyncManualResetEvent AllowFlushAsyncExit { get; } = new AsyncManualResetEvent();
+
+        protected override async ValueTask FlushAsync(CancellationToken cancellationToken)
+        {
+            this.FlushEntered.Set();
+            await this.AllowFlushAsyncExit.WaitAsync();
+            await base.FlushAsync(cancellationToken);
+        }
+    }
+
+    private class StringBase64Converter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType) => objectType == typeof(string);
+
+        public override object ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+        {
+            string decoded = Encoding.UTF8.GetString(Convert.FromBase64String((string)reader.Value!));
+            return decoded;
+        }
+
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+        {
+            var stringValue = (string?)value;
+            var encoded = Convert.ToBase64String(Encoding.UTF8.GetBytes(stringValue!));
+            writer.WriteValue(encoded);
+        }
+    }
+}
