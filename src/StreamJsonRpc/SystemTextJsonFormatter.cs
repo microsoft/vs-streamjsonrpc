47aeaff83d88c84e7c35472696a5d6abd553cfc8
Andrew Arnott <andrew.arnott@microsoft.com>
2023-04-19 18:34:57 -0600

    Add support for `Progress<T>`

--- src/StreamJsonRpc/SystemTextJsonFormatter.cs
+++ src/StreamJsonRpc/SystemTextJsonFormatter.cs
@@ -18,7 +18,7 @@ namespace StreamJsonRpc;
 /// <summary>
 /// A formatter that emits UTF-8 encoded JSON where user data should be serializable via the <see cref="JsonSerializer"/>.
 /// </summary>
-public partial class SystemTextJsonFormatter : IJsonRpcMessageFormatter, IJsonRpcMessageTextFormatter
+public partial class SystemTextJsonFormatter : FormatterBase, IJsonRpcMessageFormatter, IJsonRpcMessageTextFormatter, IJsonRpcInstanceContainer, IJsonRpcFormatterState
 {
     private static readonly JsonWriterOptions WriterOptions = new() { };
 
@@ -53,7 +53,7 @@ public partial class SystemTextJsonFormatter : IJsonRpcMessageFormatter, IJsonRp
             IncludeFields = true,
 
             // Provide compatibility with DataContractSerializer attributes by default.
-            TypeInfoResolver = DataContractResolver.OnlyDecoratedTypes,
+            TypeInfoResolver = new DataContractResolver(onlyRecognizeDecoratedTypes: true),
         });
     }
 
@@ -89,7 +89,7 @@ public partial class SystemTextJsonFormatter : IJsonRpcMessageFormatter, IJsonRp
             throw new JsonException("Expected a JSON object at the root of the message.");
         }
 
-        JsonRpcMessage returnValue;
+        JsonRpcMessage message;
         if (document.RootElement.TryGetProperty(Utf8Strings.method, out JsonElement methodElement))
         {
             JsonRpcRequest request = new(this)
@@ -98,7 +98,7 @@ public partial class SystemTextJsonFormatter : IJsonRpcMessageFormatter, IJsonRp
                 Method = methodElement.GetString(),
                 JsonArguments = document.RootElement.TryGetProperty(Utf8Strings.@params, out JsonElement paramsElement) ? paramsElement : null,
             };
-            returnValue = request;
+            message = request;
         }
         else if (document.RootElement.TryGetProperty(Utf8Strings.result, out JsonElement resultElement))
         {
@@ -107,7 +107,7 @@ public partial class SystemTextJsonFormatter : IJsonRpcMessageFormatter, IJsonRp
                 RequestId = ReadRequestId(),
                 JsonResult = resultElement,
             };
-            returnValue = result;
+            message = result;
         }
         else if (document.RootElement.TryGetProperty(Utf8Strings.error, out JsonElement errorElement))
         {
@@ -122,7 +122,7 @@ public partial class SystemTextJsonFormatter : IJsonRpcMessageFormatter, IJsonRp
                 },
             };
 
-            returnValue = error;
+            message = error;
         }
         else
         {
@@ -131,22 +131,24 @@ public partial class SystemTextJsonFormatter : IJsonRpcMessageFormatter, IJsonRp
 
         if (document.RootElement.TryGetProperty(Utf8Strings.jsonrpc, out JsonElement jsonRpcElement))
         {
-            returnValue.Version = jsonRpcElement.ValueEquals(Utf8Strings.v2_0) ? "2.0" : (jsonRpcElement.GetString() ?? throw new JsonException("Unexpected null value for jsonrpc property."));
+            message.Version = jsonRpcElement.ValueEquals(Utf8Strings.v2_0) ? "2.0" : (jsonRpcElement.GetString() ?? throw new JsonException("Unexpected null value for jsonrpc property."));
         }
         else
         {
             // Version 1.0 is implied when it is absent.
-            returnValue.Version = "1.0";
+            message.Version = "1.0";
         }
 
         RequestId ReadRequestId()
         {
             return document.RootElement.TryGetProperty(Utf8Strings.id, out JsonElement idElement)
                 ? idElement.Deserialize<RequestId>(BuiltInSerializerOptions)
-                : RequestId.NotSpecified;
+            : RequestId.NotSpecified;
         }
 
-        return returnValue;
+        this.TryHandleSpecialIncomingMessage(message);
+
+        return message;
     }
 
     /// <inheritdoc/>
@@ -158,121 +160,129 @@ public partial class SystemTextJsonFormatter : IJsonRpcMessageFormatter, IJsonRp
     /// <inheritdoc/>
     public void Serialize(IBufferWriter<byte> bufferWriter, JsonRpcMessage message)
     {
-        using Utf8JsonWriter writer = new(bufferWriter, WriterOptions);
-        writer.WriteStartObject();
-        WriteVersion();
-        switch (message)
+        using (this.TrackSerialization(message))
         {
-            case Protocol.JsonRpcRequest request:
-                WriteId(request.RequestId);
-                writer.WriteString(Utf8Strings.method, request.Method);
-                WriteArguments(request);
-                writer.WriteEndObject();
-                break;
-            case Protocol.JsonRpcResult result:
-                WriteId(result.RequestId);
-                WriteResult(result);
-                writer.WriteEndObject();
-                break;
-            case Protocol.JsonRpcError error:
-                WriteId(error.RequestId);
-                WriteError(error);
-                writer.WriteEndObject();
-                break;
-            default:
-                throw new ArgumentException("Unknown message type: " + message.GetType().Name, nameof(message));
-        }
-
-        void WriteVersion()
-        {
-            switch (message.Version)
+            using Utf8JsonWriter writer = new(bufferWriter, WriterOptions);
+            writer.WriteStartObject();
+            WriteVersion();
+            switch (message)
             {
-                case "1.0":
-                    // The 1.0 protocol didn't include the version property at all.
+                case Protocol.JsonRpcRequest request:
+                    WriteId(request.RequestId);
+                    writer.WriteString(Utf8Strings.method, request.Method);
+                    WriteArguments(request);
+                    writer.WriteEndObject();
                     break;
-                case "2.0":
-                    writer.WriteString(Utf8Strings.jsonrpc, Utf8Strings.v2_0);
+                case Protocol.JsonRpcResult result:
+                    WriteId(result.RequestId);
+                    WriteResult(result);
+                    writer.WriteEndObject();
+                    break;
+                case Protocol.JsonRpcError error:
+                    WriteId(error.RequestId);
+                    WriteError(error);
+                    writer.WriteEndObject();
                     break;
                 default:
-                    writer.WriteString(Utf8Strings.jsonrpc, message.Version);
-                    break;
+                    throw new ArgumentException("Unknown message type: " + message.GetType().Name, nameof(message));
             }
-        }
 
-        void WriteId(RequestId id)
-        {
-            if (!id.IsEmpty)
+            void WriteVersion()
             {
-                writer.WritePropertyName(Utf8Strings.id);
-                RequestIdJsonConverter.Instance.Write(writer, id, BuiltInSerializerOptions);
-            }
-        }
-
-        void WriteArguments(Protocol.JsonRpcRequest request)
-        {
-            if (request.ArgumentsList is not null)
-            {
-                writer.WriteStartArray(Utf8Strings.@params);
-                for (int i = 0; i < request.ArgumentsList.Count; i++)
+                switch (message.Version)
                 {
-                    WriteUserData(request.ArgumentsList[i]);
+                    case "1.0":
+                        // The 1.0 protocol didn't include the version property at all.
+                        break;
+                    case "2.0":
+                        writer.WriteString(Utf8Strings.jsonrpc, Utf8Strings.v2_0);
+                        break;
+                    default:
+                        writer.WriteString(Utf8Strings.jsonrpc, message.Version);
+                        break;
+                }
+            }
+
+            void WriteId(RequestId id)
+            {
+                if (!id.IsEmpty)
+                {
+                    writer.WritePropertyName(Utf8Strings.id);
+                    RequestIdJsonConverter.Instance.Write(writer, id, BuiltInSerializerOptions);
+                }
+            }
+
+            void WriteArguments(Protocol.JsonRpcRequest request)
+            {
+                if (request.ArgumentsList is not null)
+                {
+                    writer.WriteStartArray(Utf8Strings.@params);
+                    for (int i = 0; i < request.ArgumentsList.Count; i++)
+                    {
+                        WriteUserData(request.ArgumentsList[i]);
+                    }
+
+                    writer.WriteEndArray();
+                }
+                else if (request.NamedArguments is not null)
+                {
+                    writer.WriteStartObject(Utf8Strings.@params);
+                    foreach (KeyValuePair<string, object?> argument in request.NamedArguments)
+                    {
+                        writer.WritePropertyName(argument.Key);
+                        WriteUserData(argument.Value);
+                    }
+
+                    writer.WriteEndObject();
+                }
+                else if (request.Arguments is not null)
+                {
+                    // This is a custom named arguments object, so we'll just serialize it as-is.
+                    writer.WritePropertyName(Utf8Strings.@params);
+                    WriteUserData(request.Arguments);
+                }
+            }
+
+            void WriteResult(Protocol.JsonRpcResult result)
+            {
+                writer.WritePropertyName(Utf8Strings.result);
+                WriteUserData(result.Result);
+            }
+
+            void WriteError(Protocol.JsonRpcError error)
+            {
+                if (error.Error is null)
+                {
+                    throw new ArgumentException($"{nameof(error.Error)} property must be set.", nameof(message));
                 }
 
-                writer.WriteEndArray();
-            }
-            else if (request.NamedArguments is not null)
-            {
-                writer.WriteStartObject(Utf8Strings.@params);
-                foreach (KeyValuePair<string, object?> argument in request.NamedArguments)
+                writer.WriteStartObject(Utf8Strings.error);
+                writer.WriteNumber(Utf8Strings.code, (int)error.Error.Code);
+                writer.WriteString(Utf8Strings.message, error.Error.Message);
+                if (error.Error.Data is not null)
                 {
-                    writer.WritePropertyName(argument.Key);
-                    WriteUserData(argument.Value);
+                    writer.WritePropertyName(Utf8Strings.data);
+                    WriteUserData(error.Error.Data);
                 }
 
                 writer.WriteEndObject();
             }
-            else if (request.Arguments is not null)
+
+            void WriteUserData(object? value)
             {
-                // This is a custom named arguments object, so we'll just serialize it as-is.
-                writer.WritePropertyName(Utf8Strings.@params);
-                WriteUserData(request.Arguments);
+                JsonSerializer.Serialize(writer, value, this.massagedUserDataSerializerOptions);
             }
-        }
-
-        void WriteResult(Protocol.JsonRpcResult result)
-        {
-            writer.WritePropertyName(Utf8Strings.result);
-            WriteUserData(result.Result);
-        }
-
-        void WriteError(Protocol.JsonRpcError error)
-        {
-            if (error.Error is null)
-            {
-                throw new ArgumentException($"{nameof(error.Error)} property must be set.", nameof(message));
-            }
-
-            writer.WriteStartObject(Utf8Strings.error);
-            writer.WriteNumber(Utf8Strings.code, (int)error.Error.Code);
-            writer.WriteString(Utf8Strings.message, error.Error.Message);
-            if (error.Error.Data is not null)
-            {
-                writer.WritePropertyName(Utf8Strings.data);
-                WriteUserData(error.Error.Data);
-            }
-
-            writer.WriteEndObject();
-        }
-
-        void WriteUserData(object? value)
-        {
-            JsonSerializer.Serialize(writer, value, this.massagedUserDataSerializerOptions);
         }
     }
 
     private JsonSerializerOptions MassageUserDataSerializerOptions(JsonSerializerOptions options)
     {
+        // This is required for $/cancelRequest messages.
         options.Converters.Add(RequestIdJsonConverter.Instance);
+
+        // Add support for exotic types.
+        options.Converters.Add(new ProgressConverterFactory(this));
+
         return options;
     }
 
@@ -315,6 +325,8 @@ public partial class SystemTextJsonFormatter : IJsonRpcMessageFormatter, IJsonRp
         }
 
         public override int ArgumentCount => this.argumentCount;
+
+        internal JsonRpcMethodAttribute? ApplicableMethodAttribute { get; private set; }
 
         internal JsonElement? JsonArguments
         {
@@ -373,7 +385,10 @@ public partial class SystemTextJsonFormatter : IJsonRpcMessageFormatter, IJsonRp
 
             try
             {
-                value = valueElement?.Deserialize(typeHint ?? typeof(object), this.formatter.massagedUserDataSerializerOptions);
+                using (this.formatter.TrackDeserialization(this))
+                {
+                    value = valueElement?.Deserialize(typeHint ?? typeof(object), this.formatter.massagedUserDataSerializerOptions);
+                }
             }
             catch (JsonException ex)
             {
@@ -559,17 +574,69 @@ public partial class SystemTextJsonFormatter : IJsonRpcMessageFormatter, IJsonRp
         }
     }
 
+    private class ProgressConverterFactory : JsonConverterFactory
+    {
+        private readonly SystemTextJsonFormatter formatter;
+
+        internal ProgressConverterFactory(SystemTextJsonFormatter formatter)
+        {
+            this.formatter = formatter;
+        }
+
+        public override bool CanConvert(Type typeToConvert) => MessageFormatterProgressTracker.CanSerialize(typeToConvert) || MessageFormatterProgressTracker.CanDeserialize(typeToConvert);
+
+        public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+        {
+            Type progressType = typeToConvert.GetGenericArguments()[0];
+            Type converterType = typeof(Converter<>).MakeGenericType(progressType);
+            return (JsonConverter)Activator.CreateInstance(converterType, this.formatter)!;
+        }
+
+        private class Converter<T> : JsonConverter<IProgress<T>?>
+        {
+            private readonly SystemTextJsonFormatter formatter;
+
+            public Converter(SystemTextJsonFormatter formatter)
+            {
+                this.formatter = formatter;
+            }
+
+            public override IProgress<T>? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            {
+                if (reader.TokenType == JsonTokenType.Null)
+                {
+                    return null;
+                }
+
+                Assumes.NotNull(this.formatter.JsonRpc);
+                object token = reader.TokenType switch
+                {
+                    JsonTokenType.String => reader.GetString()!,
+                    JsonTokenType.Number => reader.GetInt64(),
+                    _ => throw new NotSupportedException("Unsupported token type."), // Ideally, we should *copy* the token so we can retain it and replay it later.
+                };
+
+                bool clientRequiresNamedArgs = this.formatter.DeserializingMessage is JsonRpcRequest { ApplicableMethodAttribute: { ClientRequiresNamedArguments: true } };
+                return (IProgress<T>?)this.formatter.FormatterProgressTracker.CreateProgress(this.formatter.JsonRpc, token, typeToConvert, clientRequiresNamedArgs);
+            }
+
+            public override void Write(Utf8JsonWriter writer, IProgress<T>? value, JsonSerializerOptions options)
+            {
+                long progressId = this.formatter.FormatterProgressTracker.GetTokenForProgress(value!);
+                writer.WriteNumberValue(progressId);
+            }
+        }
+    }
+
     private class DataContractResolver : IJsonTypeInfoResolver
     {
-        internal static readonly DataContractResolver OnlyDecoratedTypes = new(onlyRecognizeDecoratedTypes: true);
-
         private readonly Dictionary<Type, JsonTypeInfo?> typeInfoCache = new();
 
         private readonly bool onlyRecognizeDecoratedTypes;
 
         private readonly DefaultJsonTypeInfoResolver fallbackResolver = new();
 
-        private DataContractResolver(bool onlyRecognizeDecoratedTypes)
+        internal DataContractResolver(bool onlyRecognizeDecoratedTypes)
         {
             this.onlyRecognizeDecoratedTypes = onlyRecognizeDecoratedTypes;
         }
