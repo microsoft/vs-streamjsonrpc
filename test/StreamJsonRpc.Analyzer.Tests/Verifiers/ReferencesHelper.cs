ec5097d9086502ee66984ce4a6b91b5405bbecc1
Andrew Arnott <andrew.arnott@microsoft.com>
2025-10-13 15:14:21 -0600

    Fix analyzer tests to not pull from nuget.org

--- test/StreamJsonRpc.Analyzer.Tests/Verifiers/ReferencesHelper.cs
+++ test/StreamJsonRpc.Analyzer.Tests/Verifiers/ReferencesHelper.cs
@@ -6,10 +6,14 @@ using PolyType;
 
 internal static class ReferencesHelper
 {
+    internal static readonly string NuGetConfigPath = FindNuGetConfigPath();
+
 #if NET
-    internal static ReferenceAssemblies References = ReferenceAssemblies.Net.Net80;
+    internal static ReferenceAssemblies References = ReferenceAssemblies.Net.Net80
+        .WithNuGetConfigFilePath(NuGetConfigPath);
 #else
     internal static ReferenceAssemblies References = ReferenceAssemblies.NetStandard.NetStandard20
+        .WithNuGetConfigFilePath(NuGetConfigPath)
         .AddPackages([
             new PackageIdentity("System.Memory", "4.6.3"),
             new PackageIdentity("System.Threading.Tasks.Extensions", "4.6.1"),
@@ -22,5 +26,22 @@ internal static class ReferencesHelper
         yield return MetadataReference.CreateFromFile(typeof(JsonRpc).Assembly.Location);
         yield return MetadataReference.CreateFromFile(typeof(GenerateShapeAttribute).Assembly.Location);
         yield return MetadataReference.CreateFromFile(typeof(IDisposableObservable).Assembly.Location);
+    }
+
+    private static string FindNuGetConfigPath()
+    {
+        string? path = AppContext.BaseDirectory;
+        while (path is not null)
+        {
+            string candidate = Path.Combine(path, "nuget.config");
+            if (File.Exists(candidate))
+            {
+                return candidate;
+            }
+
+            path = Path.GetDirectoryName(path);
+        }
+
+        throw new InvalidOperationException("Could not find NuGet.config by searching up from " + AppContext.BaseDirectory);
     }
 }
