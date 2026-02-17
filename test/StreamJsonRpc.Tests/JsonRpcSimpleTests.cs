fc21c3be1a1a64df1e7177592a7c278a620264a0
Andrew Arnott <andrew.arnott@microsoft.com>
2026-02-17 09:31:09 -0700

    Add the `string JsonRpc.DisplayName` property

---
+++ test/StreamJsonRpc.Tests/JsonRpcSimpleTests.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+/// <summary>
+/// <see cref="JsonRpc"/> tests that don't exercise the <see cref="IJsonRpcMessageHandler"/> or <see cref="IJsonRpcMessageFormatter"/>.
+/// </summary>
+public class JsonRpcSimpleTests
+{
+    [Fact]
+    public void DisplayName_Default()
+    {
+        JsonRpc jsonRpc = new JsonRpc(Stream.Null);
+        Assert.Null(jsonRpc.DisplayName);
+    }
+
+    [Fact]
+    public void DisplayName_Settable()
+    {
+        JsonRpc jsonRpc = new(Stream.Null)
+        {
+            DisplayName = "Test name",
+        };
+
+        Assert.Equal("Test name", jsonRpc.DisplayName);
+    }
+}
